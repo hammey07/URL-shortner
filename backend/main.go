37c0d6c483848467165b9ec30109b6c4372279cd
Hammad Saleem <hammadsaleem@Hammads-MacBook-Air.local>
2025-06-24 22:15:11 +0100

    basic backend ready

---
+++ backend/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+var urlStore = make(map[string]string)
+var counter = 1000
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	originalURL := r.URL.Query().Get("url")
+	if originalURL == "" {
+		http.Error(w, "Missing ?url=parameter", http.StatusBadRequest)
+		return
+	}
+
+	counter++
+	shortCode := strconv.Itoa(counter)
+
+	urlStore[shortCode] = originalURL
+
+	shortURL := "http://localhost:8080/" + shortCode
+	fmt.Fprintf(w, "Short URL: %s\n", shortURL)
+}
+
+func main() {
+	http.HandleFunc("/shorten", helloHandler)
+	fmt.Print("Server Running on http://localhost:8080")
+	http.HandleFunc("/", redirectHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	code := r.URL.Path[1:]
+	originalURL, ok := urlStore[code]
+	if !ok {
+		http.NotFound(w, r)
+		return
+	}
+
+	http.Redirect(w, r, originalURL, http.StatusFound)
+}
---
+++ frontend/README.md
@@ -0,0 +1,9 @@
+# URL Shortner Web application
+
+A simple application that lets user shortens URL.
+
+## Tech Stack:
+#### Frontend: ReactJS & TypeScript
+#### Backend: Go Language
+
+## Note: This application is a work in progress.  
